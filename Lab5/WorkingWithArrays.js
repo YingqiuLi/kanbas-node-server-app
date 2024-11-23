c3e7a0b4dadb1b66e4034bbf0022a0c1a26432ae
YingqiuLi <li.yingq@northeastern.edu>
2024-11-22 22:05:37 -0500

    first commit

---
+++ Lab5/WorkingWithArrays.js
@@ -0,0 +1,82 @@
+let todos = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: true },
+  { id: 3, title: "Task 3", completed: false },
+  { id: 4, title: "Task 4", completed: true },
+];
+
+export default function WorkingWithArrays(app) {
+  // Create a new todo
+  app.get("/lab5/todos/create", (req, res) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      title: "New Task",
+      completed: false,
+    };
+    todos.push(newTodo);
+    res.json(todos);
+  });
+
+  app.post("/lab5/todos", (req, res) => {
+    const newTodo = { ...req.body, id: new Date().getTime() };
+    todos.push(newTodo);
+    res.json(newTodo);
+  });
+
+  // Retrieve a todo by ID
+
+  // Update a todo's title
+  app.get("/lab5/todos/:id/title/:title", (req, res) => {
+    const { id, title } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (todo) {
+      todo.title = title;
+    }
+    res.json(todos);
+  });
+
+  // Update a todo's description
+  app.get("/lab5/todos/:id/description/:description", (req, res) => {
+    const { id, description } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (todo) {
+      todo.description = description;
+    }
+    res.json(todos);
+  });
+
+  // Update a todo's completed status
+  app.get("/lab5/todos/:id/completed/:completed", (req, res) => {
+    const { id, completed } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (todo) {
+      todo.completed = completed === "true";
+    }
+    res.json(todos);
+  });
+
+  // Delete a todo by ID
+  app.get("/lab5/todos/:id/delete", (req, res) => {
+    const { id } = req.params;
+    todos = todos.filter((t) => t.id !== parseInt(id));
+    res.json(todos);
+  });
+
+  app.get("/lab5/todos/:id/delete", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    const todoIndex = todos.indexOf(todo);
+    if (todoIndex !== -1) {
+      todos.splice(todoIndex, 1);
+    }
+    res.json(todos);
+  });
+  
+  app.delete("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    todos.splice(todoIndex, 1);
+    res.sendStatus(200);
+  });
+
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
